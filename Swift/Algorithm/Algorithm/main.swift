e1584924e92bb0a74c4840793bcfe86932648e5a
Sett Park <park9368@naver.com>
2021-08-16 14:14:20 +0900

    complete AlgorithmDay20 (brute force)

--- Swift/Algorithm/Algorithm/main.swift
+++ Swift/Algorithm/Algorithm/main.swift
@@ -7,5 +7,5 @@
 
 import Foundation
 
-let a = ValidateStackSequences()
-print(a.validateStackSequences([1,2,3,4,5], [4,5,3,2,1]))
+let a = ContainerWithMostWater()
+print(a.maxArea([2,3,4,5,18,17,6]))
