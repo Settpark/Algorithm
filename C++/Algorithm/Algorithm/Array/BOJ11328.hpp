cbd668a04715f2fb08f06bcdf14425e361f829a5
Sett Park <park9368@naver.com>
2021-09-21 03:30:19 +0900

    solved BOJ2577, BOJ11328 with C++

---
+++ C++/Algorithm/Algorithm/Array/BOJ11328.hpp
@@ -0,0 +1,21 @@
+//
+//  BOJ11328.hpp
+//  Algorithm
+//
+//  Created by 박정하 on 2021/09/21.
+//
+
+#ifndef BOJ11328_hpp
+#define BOJ11328_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class BOJ11328 {
+    void solution();
+};
+
+#endif /* BOJ11328_hpp */
---
+++ C++/Algorithm/Algorithm/Array/BOJ2577.hpp
@@ -0,0 +1,20 @@
+//
+//  BOJ2577.hpp
+//  Algorithm
+//
+//  Created by 박정하 on 2021/09/21.
+//
+
+#ifndef BOJ2577_hpp
+#define BOJ2577_hpp
+
+#include <iostream>
+
+using namespace std;
+
+class BOJ2577 {
+public:
+    void solutionBOJ2577();
+};
+
+#endif /* BOJ2577_hpp */
