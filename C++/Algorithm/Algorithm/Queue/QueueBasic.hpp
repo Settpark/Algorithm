0aa2b7ed7711126d56e66706dc88b1e5036e0bc2
Sett Park <park9368@naver.com>
2021-09-24 02:15:02 +0900

    solved BOJ10845

---
+++ C++/Algorithm/Algorithm/Queue/QueueBasic.hpp
@@ -0,0 +1,24 @@
+//
+//  QueueBasic.hpp
+//  Algorithm
+//
+//  Created by 박정하 on 2021/09/24.
+//
+
+#ifndef QueueBasic_hpp
+#define QueueBasic_hpp
+
+#include <iostream>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+
+class QueueBasic {
+public:
+    void solution();
+};
+
+
+#endif /* QueueBasic_hpp */
