79ddedff82ba0fa2c9fa593eb70d7f86c2ebce98
Sett Park <park9368@naver.com>
2021-09-27 02:01:31 +0900

    solved BOJ2178

---
+++ C++/Algorithm/Algorithm/BFS/BOJ2178.hpp
@@ -0,0 +1,22 @@
+//
+//  BOJ2178.hpp
+//  Algorithm
+//
+//  Created by 박정하 on 2021/09/27.
+//
+
+#ifndef BOJ2178_hpp
+#define BOJ2178_hpp
+
+#include <iostream>
+#include <queue>
+#include <utility>
+
+using namespace std;
+
+class BOJ2178 {
+public:
+    void solution();
+};
+
+#endif /* BOJ2178_hpp */
