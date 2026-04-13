9d2abbe17e21e2499d1129437b335436f545cfff
settpark <park9368@naver.com>
2026-04-13 15:15:22 +0900

    resolved boj 19942

--- VSCodeC++/main.cpp
+++ VSCodeC++/main.cpp
@@ -2,68 +2,62 @@
 using namespace std;
 
 int n;
-vector<int> coin(21); //c 스타일의 배열은 얕은 복사가 발생함
+int mp, mf, ms, mv;
+vector<vector<int>> inputs(16);
+vector<pair<int, vector<int>>> answers;
 
-int main()
-{
-    cin >> n; // n은 20이하, 뒷면이 보이는 개수를 최소화
+int main() {
+    cin >> n;
+    cin >> mp >> mf >> ms >> mv;
+
+    for (int i = 0; i < n; i++) {
+        int pi, fi, si, vi, ci;
+        cin >> pi >> fi >> si >> vi >> ci;
+        inputs[i].push_back(pi);
+        inputs[i].push_back(fi);
+        inputs[i].push_back(si);
+        inputs[i].push_back(vi);
+        inputs[i].push_back(ci);
+    }
+
+    for (int i = 0; i < (1 << n); i++) {
+        int a = 0, b = 0, c = 0, d = 0, e = 0;
+        vector<int> answerIdxs;
+        for (int j = 0; j < n; j++) {
+            int targetIdx = (1 << j);
+            if (i & targetIdx) {
+                answerIdxs.push_back(j + 1);
+                a += inputs[j][0];
+                b += inputs[j][1];
+                c += inputs[j][2];
+                d += inputs[j][3];
+                e += inputs[j][4];
+                if (a >= mp && b >= mf && c >= ms && d >= mv) {
+                    answers.push_back({e, answerIdxs});
+                }
+            }
+        }
+    }
+    if (answers.size() == 0) {
+        cout << -1;
+        return 0;
+    }
+
     int minimum = 987654321;
-    for (int i = 0; i < n; i++)
-    {
-        int k = 0;
-        for (int j = 0; j < n; j++)
-        {
-            char l;
-            cin >> l;
-            if (l == 'T')
-            {
-                k |= (1 << j);
+    vector<int> finalAnswer;
+    for (auto e: answers) {
+        if (e.first < minimum) {
+            minimum = e.first;
+            finalAnswer = e.second;
+        } else if (e.first == minimum) {
+            if (finalAnswer > e.second) {
+                finalAnswer = e.second;
             }
         }
-        coin[i] = k;
     }
-
-    for (int mask = 0; mask < (1 << n); mask++)
-    {
-        auto copied = coin;
-        for (int s = 0; s < n; s++)
-        {
-            int shifted = 1 << s;
-            if (mask & shifted)
-            {
-                copied[s] ^= (1 << n) - 1;
-            }
-        } //행을 뒤집는 연산식
-        for (int a = 0; a < n; a++)
-        {
-            int tailCount = 0;
-            int targetBit = (1 << a);
-            for (int b = 0; b < n; b++)
-            {
-                if (copied[b] & targetBit)
-                {
-                    tailCount++;
-                }
-            }
-            if (tailCount > n / 2)
-            {
-                for (int c = 0; c < n; c++)
-                {
-                    copied[c] ^= targetBit;
-                }
-            }
-        }
-        int currentCount = 0;
-        for (int d = 0; d < n; d++) {
-            for (int e = 0; e<n; e++) {
-                int t = (1 << e);
-                if ((copied[d] & t)) {
-                    currentCount++;
-                }
-            }
-        }
-        minimum = min(currentCount, minimum);
+    cout << minimum << '\n';
+    for (auto e: finalAnswer) {
+        cout << e << ' ';
     }
-    cout << minimum;
     return 0;
 }
