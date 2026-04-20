bc65ca74dc47a9c36004d190a033059de27b6527
settpark <park9368@naver.com>
2026-04-20 14:09:17 +0900

    solve boj_2178

---
+++ VSCodeC++/BOJ/boj_2178.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+int arr[101][101];
+int isvisited[101][101];
+
+queue<pair<int, int>> q;
+
+int main() {
+    cin >> N >> M;
+
+    for (int y = 0; y < N; y++) {
+        string m;
+        cin >> m;
+        for (int x = 0; x < M; x++) {
+            char k = m[x];
+            int toInt = k - '0';
+            arr[y][x] = toInt;
+        }
+    }
+
+    q.push({0, 0});
+    isvisited[0][0] = 1;
+
+    while (!q.empty()) {
+        auto front = q.front();
+        q.pop();
+        for (int i = 0; i < 4; i++) {
+            int ny = front.first + dy[i];
+            int nx = front.second + dx[i];
+
+            if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
+            if (isvisited[ny][nx] || arr[ny][nx] == 0) continue;
+            q.push({ny, nx});
+            isvisited[ny][nx] = isvisited[front.first][front.second] + 1;
+        }
+    }
+    cout << isvisited[N - 1][M - 1] << endl;
+}
+//     }
+// }
+
+// 5 5
+// 0 0
+// 4 4
+// 1 0 1 0 1
+// 1 1 1 0 1
+// 0 0 1 1 1
+// 0 0 1 1 1
+// 0 0 1 1 1
--- VSCodeC++/main.cpp
+++ VSCodeC++/main.cpp
@@ -1,61 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, M;
-
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
-
-int arr[101][101];
-int isvisited[101][101];
-
-queue<pair<int, int>> q;
-
 int main() {
-    cin >> N >> M;
-
-    int startX, startY;
-    cin >> startY >> startX;
-
-    int destX, destY;
-    cin >> destY >> destX;
-
-    for (int y = 0; y < N; y++) {
-        for (int x = 0; x < M; x++) {
-            cin >> arr[y][x];
-        }
-    }
-    // for (int i = 0; i < N; i++) {
-    //     for (int j = 0; j < M; j++) {
-    //         if (arr[i][j] == 1 && isvisited[i][j] == false) {
-    //             q.push({i, j});
-    //             isvisited[i][j] = true;
-    q.push({startY, startX});
-    isvisited[startY][startX] = 1;
-
-    while (!q.empty()) {
-        auto front = q.front();
-        q.pop();
-        for (int i = 0; i < 4; i++) {
-            int ny = front.first + dy[i];
-            int nx = front.second + dx[i];
-
-            if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
-            if (isvisited[ny][nx] || arr[ny][nx] == 0) continue;
-            q.push({ny, nx});
-            isvisited[ny][nx] = isvisited[front.first][front.second] + 1;
-        }
-    }
-    cout << isvisited[destY][destX] << endl;
+    
 }
-//     }
-// }
-
-// 5 5
-// 0 0
-// 4 4
-// 1 0 1 0 1
-// 1 1 1 0 1
-// 0 0 1 1 1
-// 0 0 1 1 1
-// 0 0 1 1 1
