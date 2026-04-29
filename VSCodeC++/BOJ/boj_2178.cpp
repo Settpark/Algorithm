2116e7e7bdc621e0b126d07d3b92d26e7ad5d68c
settpark <park9368@naver.com>
2026-04-29 10:41:33 +0900

    resolved boj 2178

--- VSCodeC++/BOJ/boj_2178.cpp
+++ VSCodeC++/BOJ/boj_2178.cpp
@@ -2,54 +2,41 @@
 using namespace std;
 
 int N, M;
-
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
-
 int arr[101][101];
 int isvisited[101][101];
-
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, -1, 0, 1};
 queue<pair<int, int>> q;
 
 int main() {
     cin >> N >> M;
 
-    for (int y = 0; y < N; y++) {
-        string m;
-        cin >> m;
-        for (int x = 0; x < M; x++) {
-            char k = m[x];
-            int toInt = k - '0';
-            arr[y][x] = toInt;
+    for (int i = 1; i <= N; i++) {
+        string k;
+        cin >> k;
+        for (int j = 0; j < k.size(); j++) {
+            int a = k[j] - '0';
+            arr[i][j + 1] = a;
         }
     }
 
-    q.push({0, 0});
-    isvisited[0][0] = 1;
+    q.push({1, 1});
+    isvisited[1][1] = 1;
 
     while (!q.empty()) {
         auto front = q.front();
         q.pop();
+
         for (int i = 0; i < 4; i++) {
             int ny = front.first + dy[i];
             int nx = front.second + dx[i];
 
-            if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
-            if (isvisited[ny][nx] || arr[ny][nx] == 0) continue;
+            if (ny < 0 || nx < 0 || ny > N || nx > M) continue;
+            if (isvisited[ny][nx] || arr[ny][nx] != 1) continue;
             q.push({ny, nx});
             isvisited[ny][nx] = isvisited[front.first][front.second] + 1;
         }
     }
-    cout << isvisited[N - 1][M - 1] << endl;
+    
+    cout << isvisited[N][M] << endl;
 }
-//     }
-// }
-
-// 5 5
-// 0 0
-// 4 4
-// 1 0 1 0 1
-// 1 1 1 0 1
-// 0 0 1 1 1
-// 0 0 1 1 1
-// 0 0 1 1 1
