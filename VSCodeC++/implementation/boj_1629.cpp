3274ead0d007a9ad82a4c57cfe7a281c7f24448c
settpark <park9368@naver.com>
2026-05-19 10:33:22 +0900

    solved boj 1629

---
+++ VSCodeC++/implementation/boj_1629.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b, c;
+
+long long power(int a, int b) {
+    if (b == 1) return a % c;
+    long long result = power(a, b / 2);
+    result = (result * result) % c;
+    if (b % 2) result = (result * a) % c;
+    return result;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> a >> b >> c;
+
+    cout << power(a, b);
+}
